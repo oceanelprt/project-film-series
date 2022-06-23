8f3c5285de42553760ec3228a0adb61cd53eaf94
unknown <Administrateur@25SE23-2T0TVD3.ad.campus-eni.fr>
2022-06-23 14:38:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'project-films-series',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
